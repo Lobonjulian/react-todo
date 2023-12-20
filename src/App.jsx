8c04ea63140a83d2f8365eae0ce16081272f3434
Julian Lobon <julitolos6@gmail.com>
2023-12-20 15:20:55 -0500

    lista con el metodo map

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-const MyButton = () => {
-  return <button>Agregar</button>;
-};
-
-const MensajeUsuarios = () => {
-  return <h2>Bienvenidos a los nuevos usuarios</h2>;
-};
-
 const App = () => {
-  const title = "React Todo";
-  const user = true;
+  const comidasRapidas = ["🍔", "🍕", "🍟"];
 
   return (
     <div className="container">
-      <h1 className="text-primary">{title}</h1>
-      <MyButton />
-      {user ? <MensajeUsuarios /> : "no hay usuarios"}
+      <ul>
+        {comidasRapidas.map((comida, index) => (
+          <li key={index}>{comida}</li>
+        ))}
+      </ul>
     </div>
   );
 };
