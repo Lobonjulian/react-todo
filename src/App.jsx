bfb89ecaf1b3bb39d06577af5014ccc85e61ddf1
Julian Lobon <julitolos6@gmail.com>
2023-12-20 15:15:06 -0500

    Renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ const MyButton = () => {
   return <button>Agregar</button>;
 };
 
+const MensajeUsuarios = () => {
+  return <h2>Bienvenidos a los nuevos usuarios</h2>;
+};
+
 const App = () => {
   const title = "React Todo";
+  const user = true;
 
   return (
     <div className="container">
       <h1 className="text-primary">{title}</h1>
       <MyButton />
+      {user ? <MensajeUsuarios /> : "no hay usuarios"}
     </div>
   );
 };
