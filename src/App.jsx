10333792b305720569e9d1d6ca709ae1ce451eb7
Julian Lobon <julitolos6@gmail.com>
2023-12-20 15:06:34 -0500

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+const MyButton = () => {
+  return <button>Agregar</button>;
+};
+
 const App = () => {
   const title = "React Todo";
-  const classColor = {
-    primary: "text-primary",
-    info: "text-info",
-  };
 
   return (
     <div className="container">
-      <h1 className={classColor.primary}>{title}</h1>
-      <p className={classColor.info}>Lorem ipsum </p>
+      <h1 className="text-primary">{title}</h1>
+      <MyButton />
     </div>
   );
 };
