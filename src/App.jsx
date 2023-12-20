ffe381bdefb445dd0447163994952a79d3d353fb
Julian Lobon <julitolos6@gmail.com>
2023-12-20 14:57:05 -0500

    creación de expresiones JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 const App = () => {
   const title = "React Todo";
+  const classColor = {
+    primary: "text-primary",
+    info: "text-info",
+  };
 
   return (
     <div className="container">
-      <h1>{title}</h1>
+      <h1 className={classColor.primary}>{title}</h1>
+      <p className={classColor.info}>Lorem ipsum </p>
     </div>
   );
 };
